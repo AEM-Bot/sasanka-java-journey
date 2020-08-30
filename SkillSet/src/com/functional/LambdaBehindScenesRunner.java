56b9b8a30e84d32113750effea623e10ece43a35
Sasanka Reddy Chintam <schintam@virtusa.com>
2020-08-29 21:24:34 -0400

    Functional Programming

--- SkillSet/src/com/functional/LambdaBehindScenesRunner.java
+++ SkillSet/src/com/functional/LambdaBehindScenesRunner.java
@@ -3,6 +3,7 @@ package com.functional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -16,6 +17,15 @@ class EvenNumberUsinConsumer implements Consumer<Integer> {
     @Override
     public void accept(Integer integer) {
         System.out.println(integer);
+    }
+}
+
+class EvenNumbersSquareFunction implements Function<Integer, Integer> {
+
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer * integer;
     }
 }
 
@@ -41,6 +51,20 @@ public class LambdaBehindScenesRunner {
         integers.stream()
                 .filter(new EvenNumberPredicate())
                 .forEach(new EvenNumberUsinConsumer());
+
+
+        //Using lambdas map
+        integers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * n)
+                .forEach(n -> System.out.println(n));
+
+        System.out.println("====Using Functional Interface ==== ");
+        //using implementation of Function Interface
+        integers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(new EvenNumbersSquareFunction())
+                .forEach(e -> System.out.println(e));
 
     }
 }
