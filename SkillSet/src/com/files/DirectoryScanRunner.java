a403f9abd811ac60242b49b765d7ec6014e2c5c2
Sasanka Reddy Chintam <schintam@virtusa.com>
2020-08-30 17:18:54 -0400

    Directories

--- SkillSet/src/com/files/DirectoryScanRunner.java
+++ SkillSet/src/com/files/DirectoryScanRunner.java
@@ -2,12 +2,31 @@ package com.files;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public class DirectoryScanRunner {
 
     public static void main(String[] args) throws IOException {
         Files.list(Paths.get(".")).forEach(System.out::println);
 
+
+        Path currentDirectory = Paths.get(".");
+        Predicate<? super Path> predicate = path -> String.valueOf(path).endsWith("java");
+        Files.walk(currentDirectory, 6)
+                .filter(predicate)
+                .forEach(System.out::println);
+
+        BiPredicate<Path, BasicFileAttributes> matcher =
+                (path, basicFileAttributes) -> String.valueOf(path).endsWith("java");
+
+        BiPredicate<Path, BasicFileAttributes> directoryMatcher =
+                (path, basicFileAttributes) -> basicFileAttributes.isDirectory();
+
+        Files.find(currentDirectory, 5, directoryMatcher)
+                .forEach(System.out::println);
     }
 }
