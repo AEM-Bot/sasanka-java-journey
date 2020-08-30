85509677dc3327e6a866931d2084ebcc89643e43
Sasanka Reddy Chintam <schintam@virtusa.com>
2020-08-30 17:27:02 -0400

    Directories

---
+++ SkillSet/src/com/files/FileReadRunner.java
@@ -0,0 +1,12 @@
+package com.files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileReadRunner {
+    public static void main(String[] args) {
+        Path currentDirectory = Paths.get(".");
+
+
+    }
+}
